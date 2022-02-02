e9ccd7bf75aa05ea71aa43476f4c69a9f5c55da0
aratane <akunbotak503@gmail.com>
2022-02-02 21:46:53 +0700

    update

---
+++ aritmatikaku/aritmatika.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Contoh Program Aritmatika dengan PHP</title>
+</head>
+<body>
+<center>Program Sederhana Aritmatika</center>
+<form action="aksi.php" method="post">
+Nilai Pertama <input type="text" name="satu" Placeholder="Masukan Nilai Pertama"><br/>
+Pilih Operator <select name="operator">
+<option>Pilih Salah Satu !</option>
+<option>Penjumlahan</option>
+<option>Pengurangan</option>
+<option>Perkalian</option>
+<option>Pembagian</option>
+</select>
+Nilai Kedua <input type="text" name="dua" Placeholder="Masukan Nilai Kedua"><br/>
+<input type="reset" Value="Batal">
+<input type="submit" Value="Hitung">
+</form>
+</body>
+</html>
